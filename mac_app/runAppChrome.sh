088f7918ab179375912ed7ea10c4a063209eff79
Pierre-Alexandre Veyry <paveyry@arista.com>
2020-01-31 16:17:03 +0100

    mac_app: remove chrome-based mac app

--- mac_app/runAppChrome.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Applications/BetterChat.app/Contents/Resources/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir=$HOME/.betterchat 'data:text/html;charset=utf-8,<!DOCTYPE html><html><body><script>alert("Please install Tampermonkey, create a new script and copy paste the desired version of gmonkeyscript.js in this instance, then close it and run BetterChat.app normally");window.location.href="https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo";</script></body></html>'
