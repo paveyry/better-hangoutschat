ae0a561b046aa71763598518eac58698d877a9c8
Pierre-Alexandre Veyry <paveyry@arista.com>
2020-09-16 15:28:14 +0100

    js: fix patch not showing with GHC build 20.8.301
    Closes issue #15

--- js/electron_code_wrapper.js
+++ js/electron_code_wrapper.js
@@ -7,12 +7,15 @@ app.on('web-contents-created', (e, webContents) => {
     }()
     )
     `;
-    webContents.on('did-finish-load', () => {
-       webContents.executeJavaScript(code, true);
-    });
-    webContents.on('before-input-event', (event, input) => {
-        if (input.key == "F12") {
-            webContents.openDevTools();
-       }
-    })
+    if (webContents.id > 3) {
+        webContents.on('did-finish-load', () => {
+            webContents.executeJavaScript(`(console.log("running better-hangoutschat..."))`);
+            webContents.executeJavaScript(code, true);
+        });
+        webContents.on('before-input-event', (event, input) => {
+            if (input.key == "F12") {
+                webContents.openDevTools();
+            }
+        });
+    }
 })
