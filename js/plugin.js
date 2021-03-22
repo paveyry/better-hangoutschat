302195b1cf6440210001e36fdce02089cf507678
Pierre-Alexandre Veyry <paveyry@arista.com>
2021-03-22 16:10:32 +0000

    js: prevent link button from multiplying

--- js/plugin.js
+++ js/plugin.js
@@ -22,6 +22,10 @@ function inject(){
     };
 
     var addLinkButton = function(searchButton) {
+        var existing = document.getElementById("displayLinkButton");
+        if (existing != null && existing.length > 0) {
+            return;
+        }
         button = document.createElement("div");
         button.innerHTML = '<div id="displayLinkButton"><div class="U26fgb mUbCce fKz7Od" title="Show thread links"><span class="xjKiLb"><span style="top: -12px"><svg viewBox="0 0 16 16" class="GfYBMd Kol3Vd V3Pk2">  <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"></path><path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"></path></svg></span></span></div></div>';
         searchButton.parentElement.insertBefore(button.childNodes[0], searchButton);
